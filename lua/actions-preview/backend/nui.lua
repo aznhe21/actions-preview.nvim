8aef8ed61823a0101b723f2308de5e866bb5459a
aznhe21 <aznhe21.mc@gmail.com>
2022-10-07 16:54:40 +0900

    chore: stylua

--- lua/actions-preview/backend/nui.lua
+++ lua/actions-preview/backend/nui.lua
@@ -20,36 +20,42 @@ function M.select(config, actions)
     },
   }))
 
-  local nui_select = Menu(vim.tbl_deep_extend("force", config.select, {
-    position = 0,
-    size = nil,
-    border = {
-      text = {
-        top = "Code Actions",
+  local nui_select = Menu(
+    vim.tbl_deep_extend("force", config.select, {
+      position = 0,
+      size = nil,
+      border = {
+        text = {
+          top = "Code Actions",
+        },
       },
-    },
-  }), {
-    lines = vim.tbl_map(function(action)
-      return Menu.item(action:title(), { action = action })
-    end, actions),
-    keymap = config.keymap,
-    on_change = function(item)
-      item.action:preview(function(preview)
-        preview = preview or { syntax = "", text = "preview not available" }
+    }),
+    {
+      lines = vim.tbl_map(function(action)
+        return Menu.item(action:title(), { action = action })
+      end, actions),
+      keymap = config.keymap,
+      on_change = function(item)
+        item.action:preview(function(preview)
+          preview = preview or { syntax = "", text = "preview not available" }
 
-        vim.api.nvim_buf_set_lines(nui_preview.bufnr, 0, -1, false, vim.split(preview.text, "\n", true))
-        vim.api.nvim_buf_set_option(nui_preview.bufnr, "syntax", preview.syntax)
-      end)
-    end,
-    on_submit = function(item)
-      item.action:apply()
-    end,
-  })
+          vim.api.nvim_buf_set_lines(nui_preview.bufnr, 0, -1, false, vim.split(preview.text, "\n", true))
+          vim.api.nvim_buf_set_option(nui_preview.bufnr, "syntax", preview.syntax)
+        end)
+      end,
+      on_submit = function(item)
+        item.action:apply()
+      end,
+    }
+  )
 
-  local layout = Layout(config.layout, Layout.Box({
-    Layout.Box(nui_preview, { size = config.preview.size }),
-    Layout.Box(nui_select, { size = config.select.size }),
-  }, { dir = config.dir }))
+  local layout = Layout(
+    config.layout,
+    Layout.Box({
+      Layout.Box(nui_preview, { size = config.preview.size }),
+      Layout.Box(nui_select, { size = config.select.size }),
+    }, { dir = config.dir })
+  )
 
   layout:mount()
   nui_select:on(event.BufLeave, function()
