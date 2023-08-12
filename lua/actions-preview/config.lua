2ab65aeadc8a448fdcdb9babbd9d3b42f124f596
bigduu <mugeng.du@qq.com>
2023-08-12 23:50:59 +0800

    update the telescope default config that same as the README write in the config file to avoid the nil exception when triiger the select method.

--- lua/actions-preview/config.lua
+++ lua/actions-preview/config.lua
@@ -1,6 +1,18 @@
 local default_config = {
   backend = { "telescope", "nui" },
-  telescope = nil,
+  telescope = {
+    sorting_strategy = "ascending",
+    layout_strategy = "vertical",
+    layout_config = {
+      width = 0.8,
+      height = 0.9,
+      prompt_position = "top",
+      preview_cutoff = 20,
+      preview_height = function(_, _, max_lines)
+        return max_lines - 15
+      end,
+    },
+  },
   nui = {
     dir = "col",
     keymap = nil,
