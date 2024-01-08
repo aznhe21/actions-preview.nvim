e02881766c786f9b6ef27edcf421ad02b3dd01d6
aznhe21 <aznhe21.mc@gmail.com>
2024-01-09 01:12:17 +0900

    feat: highlight diff in nui backend

--- lua/actions-preview/backend/nui.lua
+++ lua/actions-preview/backend/nui.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+local job_is_running = function(job_id)
+  return vim.fn.jobwait({ job_id }, 0)[1] == -1
+end
+
 function M.is_supported()
   local ok, _ = pcall(require, "nui.menu")
   return ok
@@ -10,16 +14,30 @@ function M.select(config, actions)
   local Menu = require("nui.menu")
   local Popup = require("nui.popup")
 
-  local nui_preview = Popup(vim.tbl_deep_extend("force", config.preview, {
-    size = nil,
-    border = {
-      text = {
-        top = "Code Action Preview",
+  local create_popup = function()
+    return Popup(vim.tbl_deep_extend("force", config.preview, {
+      size = nil,
+      border = {
+        text = {
+          top = "Code Action Preview",
+        },
       },
-    },
-  }))
+    }))
+  end
+  local create_layout_box = function(popup, select)
+    return Layout.Box({
+      Layout.Box(popup, { size = config.preview.size }),
+      Layout.Box(select, { size = config.select.size }),
+    }, { dir = config.dir })
+  end
 
+  local term_ids = {}
+
+  local nui_preview_blank = create_popup()
+  local nui_popups = {}
   local nui_select
+  local nui_layout
+
   nui_select = Menu(
     vim.tbl_deep_extend("force", config.select, {
       position = 0,
@@ -36,33 +54,55 @@ function M.select(config, actions)
       end, actions),
       keymap = config.keymap,
       on_change = function(item)
+        local popup = nui_popups[item.index]
+        if not popup then
+          popup = create_popup()
+          nui_popups[item] = popup
+        end
+
+        nui_layout:update(create_layout_box(popup, nui_select))
+
         item.action:preview(function(preview)
-          if nui_preview.bufnr == nil then
+          if popup.bufnr == nil then
             return
           end
 
-          preview = preview or { syntax = "", text = "preview not available" }
+          if preview and preview.cmdline then
+            if not term_ids[popup.bufnr] then
+              vim.api.nvim_buf_call(popup.bufnr, function()
+                term_ids[popup.bufnr] = vim.fn.termopen(preview.cmdline)
+              end)
+            end
+          else
+            preview = preview or { syntax = "", text = "preview not available" }
 
-          vim.api.nvim_buf_set_option(nui_preview.bufnr, "modifiable", true)
-          vim.api.nvim_buf_set_lines(nui_preview.bufnr, 0, -1, false, vim.split(preview.text, "\n", true))
-          vim.api.nvim_buf_set_option(nui_preview.bufnr, "syntax", preview.syntax)
-          vim.api.nvim_buf_set_option(nui_preview.bufnr, "modifiable", false)
+            vim.api.nvim_buf_set_lines(popup.bufnr, 0, -1, false, vim.split(preview.text, "\n", true))
+            vim.api.nvim_buf_set_option(popup.bufnr, "syntax", preview.syntax)
+            vim.api.nvim_buf_set_option(popup.bufnr, "modifiable", false)
+          end
         end)
       end,
       on_submit = function(item)
         item.action:apply()
       end,
+      on_close = function()
+        for _, term_id in pairs(term_ids) do
+          if job_is_running(term_id) then
+            vim.fn.jobstop(term_id)
+          end
+        end
+
+        nui_preview_blank:unmount()
+        for _, popup in ipairs(nui_popups) do
+          popup:unmount()
+        end
+        nui_select:unmount()
+      end,
     }
   )
 
-  local layout = Layout(
-    config.layout,
-    Layout.Box({
-      Layout.Box(nui_preview, { size = config.preview.size }),
-      Layout.Box(nui_select, { size = config.select.size }),
-    }, { dir = config.dir })
-  )
-  layout:mount()
+  nui_layout = Layout(config.layout, create_layout_box(nui_preview_blank, nui_select))
+  nui_layout:mount()
 end
 
 return M
